e49ec22058b8dc20b835b4ccb768721ac1174789
Arpit <arpitg1991@gmail.com>
2013-10-28 00:50:58 -0400

    changed groupin

--- code/groupReasonsCircumstances.R
+++ code/groupReasonsCircumstances.R
@@ -1,6 +1,31 @@
+#install.packages('useful')
+#require(cluster)
 setwd('~/stop-frisk-analysis')
-data <- load('data/cleanedData.Rdata')
-categ <- mydata[,c(43:63,65:72)]
+load('data/cleanedData.Rdata')
+categ <- df[,c(44:64,66:69)]
 categ <- data.frame(lapply(categ, as.character), stringsAsFactors=FALSE)
 write.csv(categ,'data/reasonsCircumstances.csv',row.names = FALSE)
 save(categ,file = 'data/reasonCircumstances.Rda')
+categM <- as.matrix(categ)
+categM[categM == "N"] <- 0
+categM[categM == "Y"] <- 1
+categD <- as.data.frame(categM)
+categ <- categD
+for (i in 1:ncol(categD)){
+  categ[,i] <- as.numeric(levels(categD[,i]))[categD[,i]]
+  
+}
+k2 <- kmeans(categ,2)
+k5 <- kmeans(categ,5)
+k3 <- kmeans(categ,3)
+k10 <- kmeans(categ,10)
+save(k2,k5,k3,k10,file = 'data/kmeans.Rda')
+k2<-as.matrix(as.data.frame(k2[1]))
+k3<-as.matrix(as.data.frame(k3[1]))
+k5<-as.matrix(as.data.frame(k5[1]))
+k10<-as.matrix(as.data.frame(k10[1]))
+k <- cbind(k2,k3,k5,k10)
+
+location <- data.frame(df$XCOORD,df$YCOORD)
+
+write.csv(location,'data/location.csv',row.names = FALSE)
