0b77182d106b62c904b2616de4ba2efeb5a6a533
Arpit <arpitg1991@gmail.com>
2013-10-30 00:05:26 -0400

    changing

--- code/groupReasonsCircumstances.R
+++ code/groupReasonsCircumstances.R
@@ -2,7 +2,7 @@
 #require(cluster)
 setwd('~/stop-frisk-analysis')
 load('data/cleanedData.Rdata')
-categ <- df[,c(44:64,66:69)]
+categ <- df[,c(44:72)]
 categ <- data.frame(lapply(categ, as.character), stringsAsFactors=FALSE)
 write.csv(categ,'data/reasonsCircumstances.csv',row.names = FALSE)
 save(categ,file = 'data/reasonCircumstances.Rda')
@@ -29,3 +29,18 @@ k <- cbind(k2,k3,k5,k10)
 location <- data.frame(df$XCOORD,df$YCOORD)
 
 write.csv(location,'data/location.csv',row.names = FALSE)
+##################################################################
+groups <- df[,c(109,110)]
+groups$groups <- 0
+groups$groups[which(rowSums(categ[,c(43-42,44-42,67-42,57-42,50-42)]) > 0) ] <- 1
+groups$groups[which(rowSums(categ[,c(45-42,62-42,53-42,68-42)]) > 0 )] <- 2
+groups$groups[which(rowSums(categ[,c(46-42,49-42,63-42,71-42)]) > 0 )] <- 3
+groups$groups[which(rowSums(categ[,c(56-42,58-42,60-42)]) > 0 )] <- 4
+groupings <- groups[which(groups$groups > 0 ),]
+write.csv(groupings,'data/groupings.csv',row.names = FALSE)
+############################################################################
+location <- data.frame(lapply(location, as.numeric), stringsAsFactors=FALSE)
+location <- na.omit(location)
+l4 <- kmeans(location,4)
+groups <- location
+groups$groups <- l4
