896f2a43a04b015435998b64d91a35fa857a4579
unknown <arpitg1991@gmail.com>
2013-11-23 15:17:48 -0500

    changing kmeans data

--- code/runkMeans.m
+++ code/runkMeans.m
@@ -20,7 +20,24 @@ csvwrite('Income4.csv',idx4 ) ;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 data = csvread('../data/friskCounts.csv');
 dataInd = data(:,1);
+%%
 dataK = data(:,2:size(data,2)) ; 
+
+for k = 1: size(dataK,2) 
+    dataK(:,k) = dataK(:,k) / max(dataK(:,k)) ; 
+end
+[idx4,c4,sumd4] = kmeans(dataK,4,'MaxIter',1000,'start','cluster','Replicates',10);
+[idx3,c3,sumd3] = kmeans(dataK,3,'MaxIter',1000,'start','cluster','Replicates',10);
+[idx2,c2,sumd2] = kmeans(dataK,2,'MaxIter',1000,'start','cluster','Replicates',10);
+
+idx4 = [data(:,1) idx4];
+idx3 = [data(:,1) idx3];
+idx2 = [data(:,1) idx2];
+dlmwrite('../data/Racek-4.csv',idx4,'delimiter',',','precision',7);
+dlmwrite('../data/Racek-3.csv',idx3,'delimiter',',','precision',7);
+dlmwrite('../data/Racek-2.csv',idx2,'delimiter',',','precision',7);
+%%
+dataK = data(:,[2:11 17:size(data,2)]);
 %%
 for k = 1: size(dataK,2) 
     dataK(:,k) = dataK(:,k) / max(dataK(:,k)) ; 
@@ -28,6 +45,16 @@ end
 [idx4,c4,sumd4] = kmeans(dataK,4,'MaxIter',1000,'start','cluster','Replicates',10);
 [idx3,c3,sumd3] = kmeans(dataK,3,'MaxIter',1000,'start','cluster','Replicates',10);
 [idx2,c2,sumd2] = kmeans(dataK,2,'MaxIter',1000,'start','cluster','Replicates',10);
+
+idx4 = [data(:,1) idx4];
+idx3 = [data(:,1) idx3];
+idx2 = [data(:,1) idx2];
+%%
+dlmwrite('../data/NoRacek-4.csv',idx4,'delimiter',',','precision',7);
+dlmwrite('../data/NoRacek-3.csv',idx3,'delimiter',',','precision',7);
+dlmwrite('../data/NoRacek-2.csv',idx2,'delimiter',',','precision',7);
+
+
 
 
 
