815058e29f0b6263a40fb137704c2302de14b6c4
unknown <arpitg1991@gmail.com>
2013-11-23 16:42:37 -0500

    adding factor analysis kmeans data

--- code/runkMeans.m
+++ code/runkMeans.m
@@ -53,6 +53,42 @@ idx2 = [data(:,1) idx2];
 dlmwrite('../data/NoRacek-4.csv',idx4,'delimiter',',','precision',7);
 dlmwrite('../data/NoRacek-3.csv',idx3,'delimiter',',','precision',7);
 dlmwrite('../data/NoRacek-2.csv',idx2,'delimiter',',','precision',7);
+%%
+%race with factor analysis
+dataK = data(:,2:size(data,2)) ; 
+dataK = dataK(:,[1:5 7 9:13 15 18:22 26]);
+for k = 1: size(dataK,2) 
+    dataK(:,k) = dataK(:,k) / max(dataK(:,k)) ; 
+end
+[idx4,c4,sumd4] = kmeans(dataK,4,'MaxIter',1000,'start','cluster','Replicates',10);
+[idx3,c3,sumd3] = kmeans(dataK,3,'MaxIter',1000,'start','cluster','Replicates',10);
+[idx2,c2,sumd2] = kmeans(dataK,2,'MaxIter',1000,'start','cluster','Replicates',10);
+
+idx4 = [data(:,1) idx4];
+idx3 = [data(:,1) idx3];
+idx2 = [data(:,1) idx2];
+dlmwrite('../data/Factor-Racek-4.csv',idx4,'delimiter',',','precision',7);
+dlmwrite('../data/Factor-Racek-3.csv',idx3,'delimiter',',','precision',7);
+dlmwrite('../data/Factor-Racek-2.csv',idx2,'delimiter',',','precision',7);
+%%
+%%
+%race with factor analysis
+dataK = data(:,2:size(data,2)) ; 
+dataK = dataK(:,[1:5 7 9:10  15 18:23 25 26]); 
+for k = 1: size(dataK,2) 
+    dataK(:,k) = dataK(:,k) / max(dataK(:,k)) ; 
+end
+[idx4,c4,sumd4] = kmeans(dataK,4,'MaxIter',1000,'start','cluster','Replicates',10);
+[idx3,c3,sumd3] = kmeans(dataK,3,'MaxIter',1000,'start','cluster','Replicates',10);
+[idx2,c2,sumd2] = kmeans(dataK,2,'MaxIter',1000,'start','cluster','Replicates',10);
+
+idx4 = [data(:,1) idx4];
+idx3 = [data(:,1) idx3];
+idx2 = [data(:,1) idx2];
+dlmwrite('../data/Factor-NoRacek-4.csv',idx4,'delimiter',',','precision',7);
+dlmwrite('../data/Factor-NoRacek-3.csv',idx3,'delimiter',',','precision',7);
+dlmwrite('../data/Factor-NoRacek-2.csv',idx2,'delimiter',',','precision',7);
+
 
 
 
